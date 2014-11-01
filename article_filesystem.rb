5a0bbfbe8764434b9b2a1e58c17e78f17aceccbc
Dominik <dominik.rabij@gmail.com>
2014-11-01 12:44:07 +0100

    [#13] ArticleFilesystem - loading
    + added .close in saving

--- article_filesystem.rb
+++ article_filesystem.rb
@@ -4,7 +4,19 @@ class ArticleFilesystem
   ARTICLES_FOLDER = "./articles/"
 
   def self.save_article(article)
-    file_article = File.new(ARTICLES_FOLDER + article.title.downcase.split.join("_"), "w")
-    file_article.write("#{article.author}|#{article.title}|#{article.content}|#{article.likes}|#{article.dislikes}")
+    article_file = File.new(ARTICLES_FOLDER + article.title.downcase.split.join("_"), "w")
+    article_file.write("#{article.author}|#{article.title}|#{article.content}|#{article.likes}|#{article.dislikes}")
+    article_file.close
+  end
+
+  def self.load_article(article_path)
+    article_file = File.open(ARTICLES_FOLDER + article_path, "r")
+    article_information = article_file.first.split("|")
+    loaded_article = Article.new(article_information[1], article_information[2], article_information[0])
+    article_information[3].to_i.times { loaded_article.like! }
+    article_information[4].to_i.times { loaded_article.dislike! }
+
+    article_file.close
+    loaded_article
   end
 end
